3fa17333c4104659409f30676f64d554fae385ee
DHISecurityAudit <dhi.security@doosan.com>
2023-05-18 11:14:28 +0900

    20230518 Azure Storage Service

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,17 @@
 var express = require('express');
 var router = express.Router();
-var azure = require('azure-storage'); // Guns-2023-05-17 데이터베이스 연결을 위한 변수 추가
-var tableSvc = azure.createTableService('desafelinkprdsa1','KXlVbhjynESj1sWCbXjo6LovaXe/U+IxRjbqTdZNG9aKevqSZsxGcNHpTICx6oSnqGVFtT19lPgr+AStIXPzzQ=='); // Guns-2023-05-17 데이터베이스 연결을 위한 변수 추가
+
+// 2023.05.18 Guns Azure Table Storage 상수 추가 https://learn.microsoft.com/ko-kr/azure/cosmos-db/table/how-to-use-nodejs
+const { TableServiceClient, TableClient, AzureNamedKeyCredential, odata } = require("@azure/data-tables"); 
+const endpoint = "https://desafelinkprdsa1.table.core.windows.net/Access";
+const credential = new AzureNamedKeyCredential(
+  "desafelinkprdsa1",
+  "KXlVbhjynESj1sWCbXjo6LovaXe/U+IxRjbqTdZNG9aKevqSZsxGcNHpTICx6oSnqGVFtT19lPgr+AStIXPzzQ=="
+);
+// 2023.05.18 Guns Azure Table Storage 상수 추가 https://learn.microsoft.com/ko-kr/azure/cosmos-db/table/how-to-use-nodejs
 
 var url = require('url'); // Guns-2023-04-27 변수설정을 위한 코드 수정
-const todayFullMonth = (new Date().getFullYear())*12 + new Date().getMonth()
+const todayFullMonth = (new Date().getFullYear())*12 + new Date().getMonth() // 현재 날짜 기록 관리
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -25,13 +32,11 @@ router.get('/', function(req, res, next) {
   var currentdate = d.getFullYear()+"-"+ d.getMonth() +"-"+ d.getDate()+" "+ d.getHours()+":"+ d.getMinutes()+":"+ d.getSeconds()
   
   try {
-    
-      const ip = req.headers['x-forwarded-for'];
-      dbFunc1_Insert("Access", currentdate ,Domain,req.acceptedLanguages, ip)
-  
+    dbFunc1_Insert("Access", currentdate ,Domain,req.ip,"gunsoo.jang@doosan.com" )
   }
     catch(error) {
-      dbFunc1_Insert("Access", currentdate ,"Error",error,"gunsoo.jang@doosan.com" )
+
+      dbFunc1_Insert("Access", currentdate ,"Error",error.substring(1,200),"gunsoo.jang@doosan.com" )
     // Error: someError
   }
   
@@ -43,10 +48,25 @@ router.get('/', function(req, res, next) {
 
 module.exports = router;
 
+ /* Start Guns-2023-05-18 Azure Storage Function*/  
+function dbFunc1_Insert(tableName, ST_PartitionName,ST_RowkeyName,ST_Description,ST_Info) {
 
- /* Start Guns-2023-05-17 Azure Storage Function*/  
-function dbFunc1_Insert(tableName, PartitionName,RowkeyName,KeyName,Info) {
+  const tableClient = new TableClient(
+    endpoint,
+    tableName,
+    credential
+    );
+    
+    const task = {
+      partitionKey: ST_PartitionName,
+      rowKey: ST_RowkeyName,
+      description: ST_Description,
+      Info: ST_Info
+    };
 
+    let result = tableClient.createEntity(task);
+  }
+  /*
   var entGen = azure.TableUtilities.entityGenerator;
   var task = {
      PartitionKey: entGen.String(PartitionName),
@@ -54,15 +74,7 @@ function dbFunc1_Insert(tableName, PartitionName,RowkeyName,KeyName,Info) {
      description: entGen.String(KeyName),
      Info: entGen.String(Info),
   };
+  */
   
-  
-  
-  tableSvc.insertEntity(tableName,task, function (error, result, response) {
-  if(!error){
-   // Entity inserted
-   }
-  });
-  
-  }
 
-   /* End Guns-2023-05-17 Azure Storage Function*/  
+   /* End Guns-2023-05-18 Azure Storage Function*/  
