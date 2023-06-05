2a8bfa8cb5b07ab946498c464207b0910ca1cb56
DHISecurityAudit <dhi.security@doosan.com>
2023-06-05 11:25:32 +0900

    20230605 BackEnd 완료

--- routes/index.js
+++ routes/index.js
@@ -3,69 +3,141 @@ var router = express.Router();
 
 // 2023.05.18 Guns Azure Table Storage 상수 추가 https://learn.microsoft.com/ko-kr/azure/cosmos-db/table/how-to-use-nodejs
 const { TableServiceClient, TableClient, AzureNamedKeyCredential, odata } = require("@azure/data-tables"); 
-const endpoint = "https://desafelinkprdsa1.table.core.windows.net/Access";
+const endpoint = "https://desafelinkprdsa1.table.core.windows.net";
 const credential = new AzureNamedKeyCredential(
   "desafelinkprdsa1",
   "KXlVbhjynESj1sWCbXjo6LovaXe/U+IxRjbqTdZNG9aKevqSZsxGcNHpTICx6oSnqGVFtT19lPgr+AStIXPzzQ=="
 );
 // 2023.05.18 Guns Azure Table Storage 상수 추가 https://learn.microsoft.com/ko-kr/azure/cosmos-db/table/how-to-use-nodejs
 
-var url = require('url'); // Guns-2023-04-27 변수설정을 위한 코드 수정
-const todayFullMonth = (new Date().getFullYear())*12 + new Date().getMonth() // 현재 날짜 기록 관리
+var RowKeyNum = 1; 
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
 
-  /* Start Guns-2023-04-27 변수설정을 위한 코드 수정*/
-  var fullUrl = req.url;
-  fullUrl = fullUrl.replace("/?","")
-  var Domain = fullUrl.replace("http://","")
-  Domain = Domain.replace("https://","")
-  var idx = Domain.indexOf("/")
-  if(idx >1){
-    Domain = Domain.substring(0,idx)
-  }
-  /* End Guns-2023-04-27 변수설정을 위한 코드 수정*/
 
-  /* Start Guns-2023-05-17 사용자 정보 불러오기*/ 
-  d=new Date()
-  var currentdate = d.getFullYear()+"-"+ d.getMonth() +"-"+ d.getDate()+" "+ d.getHours()+":"+ d.getMinutes()+":"+ d.getSeconds()
+    d=new Date() // Guns-2023-04-27 날짜변수 추가
+    //var currentdate = d.getFullYear()+"-"+ d.getMonth() + 1 +"-"+ d.getDate()+" "+ d.getHours()+":"+ d.getMinutes()+":"+ d.getSeconds() // Guns-2023-04-27 날짜변수 추가
+    var currentdate =  d.toISOString().slice(4,19).replace(/-/g,"");
+    currentdate =  currentdate.replace(/:/g,"");
+    
+    var fullUrl = req.url; // Guns-2023-04-27 로깅을위한 변수설정
   
-  try {
-    dbFunc1_Insert("Access", currentdate ,Domain,req.ip,"gunsoo.jang@doosan.com" )
-  }
-    catch(error) {
+    // Guns-2023-04-27 Type1: 일반사이트 접속 시, Noti 화면 구성
 
-      dbFunc1_Insert("Access", currentdate ,"Error",error.substring(1,200),"gunsoo.jang@doosan.com" )
-    // Error: someError
-  }
-  
-  /* End Guns-2023-05-17 사용자 정보 불러오기*/ 
+    fullUrl = fullUrl.replace("/?","")
+    var Domain = fullUrl.replace("http://","")
+    Domain = Domain.replace("https://","")
+    var idx = Domain.indexOf("/")
+    if(idx >1){
+      Domain = Domain.substring(0,idx)
+    }
 
-  //res.render('index', { title: 'Express' }); // Guns-2023-04-27 수정 전 코드
-  res.render('index', { reqUrl: fullUrl, reqDomain: Domain}); // Guns-2023-04-27 변수설정을 위한 코드 수정
+    var SendUserInfo = currentdate +"_" +String(RowKeyNum);
+
+    try {
+     
+      if(Domain.length < 4 || (Domain == null )) 
+      {
+        //Do nothing 
+      }
+      else if (fullUrl.endsWith(".png") || fullUrl.endsWith(".gif") || fullUrl.endsWith(".jpg")) {
+        console.log("이미지 파일임")
+      }
+      else{
+      RowKeyNum = RowKeyNum + 1;
+      /* Start Guns-2023-05-23 사용자 정보 불러오기*/ 
+
+      
+
+      dbFunc1_Insert("Access", currentdate, //tableName, ST_PartitionName
+      SendUserInfo, //,ST_RowkeyName,
+      String(req.header('User-Agent')), // ST_BrowserInfo,
+      String(req.header('x-forwarded-for')), // ST_IPHeader,
+      Domain, fullUrl) // ST_Domain,ST_FullUrl
+
+      if (RowKeyNum > 10000000){
+          RowKeyNum = 1;
+        }
+      
+      }
+
+    
+    }
+    catch(error) { // 에러로그 Azure Table에 반영
+
+      if(error.length > 250){
+        var reduceError = error.slice(0,200)
+        dbFunc3_InsertError("Error",currentdate,//tableName, Err_PartitionName
+        SendUserInfo, // Err_RowkeyName
+        reduceError // Err_Descripion
+        )
+      }
+      else{
+        dbFunc3_InsertError("Error",currentdate,//tableName, Err_PartitionName
+        SendUserInfo, // Err_RowkeyName
+        error // Err_Descripion
+        )
+      }
+    
+    }
+    /* End Guns-2023-05-23 사용자 정보 불러오기*/ 
+
+    res.render('index', { reqUrl: fullUrl, reqDomain: Domain, reqUserInfo: SendUserInfo}); // Guns-2023-04-27 변수설정을 위한 코드 수정
+   
 });
 
 module.exports = router;
 
- /* Start Guns-2023-05-18 Azure Storage Function*/  
-function dbFunc1_Insert(tableName, ST_PartitionName,ST_RowkeyName,ST_Description,ST_Info) {
-
-  const tableClient = new TableClient(
-    endpoint,
-    tableName,
-    credential
-    );
-    
-    const task = {
-      partitionKey: ST_PartitionName,
-      rowKey: ST_RowkeyName,
-      description: ST_Description,
-      Info: ST_Info
-    };
-
-    let result = tableClient.createEntity(task);
+ /* Start Guns-2023-05-18 Azure Storage Function
+   tableName, ST_PartitionName(yyyymmdd), ST_RowkeyName (SeqNumber),ST_Description(ST_FullUrl),ST_BrowserInfo, ST_IPHeader, ST_Domain, 
+ */  
+function dbFunc1_Insert(tableName, ST_PartitionName,ST_RowkeyName,ST_BrowserInfo,ST_IPHeader,ST_Domain,ST_FullUrl) {
+  try{
+    const tableClient = new TableClient(
+      endpoint,
+      tableName,
+      credential
+      );
+      
+      const task = {
+        partitionKey: ST_PartitionName, // yyyymmdd
+        rowKey: ST_RowkeyName, // (SeqNumber)
+        description: ST_FullUrl, // ST_FullUrl
+        BrowserInfo : ST_BrowserInfo,
+        IPHeader : ST_IPHeader,
+        Domain : ST_Domain
+      };
+  
+      let result = tableClient.createEntity(task);
   }
+  catch(error) {
+    console.log(error)
+  }
+}
+
+function dbFunc3_InsertError(tableName, Err_PartitionName,Err_RowkeyName, Err_Descripion) {
+  try{
+    const tableClient = new TableClient(
+      endpoint,
+      tableName,
+      credential
+      );
+      
+      const task = {
+        partitionKey: Err_PartitionName,
+        rowKey: Err_RowkeyName,
+        description: Err_Descripion
+      };
+  
+      let result = tableClient.createEntity(task);
+  }
+  catch(error) {
+    console.log(error)
+  }
+}
+
+
   /*
   var entGen = azure.TableUtilities.entityGenerator;
   var task = {
--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,99 @@
 var express = require('express');
 var router = express.Router();
 
+// 2023.05.18 Guns Azure Table Storage 상수 추가 https://learn.microsoft.com/ko-kr/azure/cosmos-db/table/how-to-use-nodejs
+const { TableServiceClient, TableClient, AzureNamedKeyCredential, odata } = require("@azure/data-tables"); 
+const endpoint = "https://desafelinkprdsa1.table.core.windows.net";
+const credential = new AzureNamedKeyCredential(
+  "desafelinkprdsa1",
+  "KXlVbhjynESj1sWCbXjo6LovaXe/U+IxRjbqTdZNG9aKevqSZsxGcNHpTICx6oSnqGVFtT19lPgr+AStIXPzzQ=="
+);
+// 2023.05.18 Guns Azure Table Storage 상수 추가 https://learn.microsoft.com/ko-kr/azure/cosmos-db/table/how-to-use-nodejs
+
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+
+  // Guns-2023-06-05 Continue 로그 -- start
+  var fullUrl_user = req.url; 
+  try {
+    res.send('respond with a resource' + fullUrl_user);
+   
+
+    d_user=new Date() // Guns-2023-04-27 날짜변수 추가
+    var currentdate_user =  d_user.toISOString().slice(4,19).replace(/-/g,"");
+
+    fullUrl_user = fullUrl_user.replace("/?","")
+    console.log('fullUrl ==>' + fullUrl_user); // 
+    var splitFullURL = fullUrl_user.split("_fu_"); //index.ejs에서 정보를 반영하기 위한
+    console.log('splitFullURL[0] ==>' + splitFullURL[0]); // 
+    console.log('splitFullURL[1] ==>' + splitFullURL[1]); // 
+    
+    dbFunc2_InsertContinue("Continue", currentdate_user, //tableName, ST_PartitionName
+    splitFullURL[0], //,ST_RowkeyName,
+    splitFullURL[1]) // ST_Descripion
+
+  }
+  catch(error) {
+    if(error.length > 250){
+      var reduceError = error.slice(0,200)
+      dbFunc3_InsertError("Error",currentdate,//tableName, Err_PartitionName
+      SendUserInfo, // Err_RowkeyName
+      reduceError // Err_Descripion
+      )
+    }
+    else{
+      dbFunc3_InsertError("Error",currentdate,//tableName, Err_PartitionName
+      SendUserInfo, // Err_RowkeyName
+      error // Err_Descripion
+      )
+    }
+  }
+  // Guns-2023-06-05 Continue 로그 -- end
+
+
 });
 
 module.exports = router;
+
+// 2023.05.26 Guns Continue 로그 추가
+function dbFunc2_InsertContinue(tableName, ST_PartitionName,ST_RowkeyName, ST_Descripion) {
+  try{
+    const tableClient = new TableClient(
+      endpoint,
+      tableName,
+      credential
+      );
+      
+      const task = {
+        partitionKey: ST_PartitionName,
+        rowKey: ST_RowkeyName,
+        description: ST_Descripion
+      };
+  
+      let result = tableClient.createEntity(task);
+  }
+  catch(error) {
+    console.log(error)
+  }
+}
+
+function dbFunc3_InsertError(tableName, Err_PartitionName,Err_RowkeyName, Err_Descripion) {
+  try{
+    const tableClient = new TableClient(
+      endpoint,
+      tableName,
+      credential
+      );
+      
+      const task = {
+        partitionKey: Err_PartitionName,
+        rowKey: Err_RowkeyName,
+        description: Err_Descripion
+      };
+  
+      let result = tableClient.createEntity(task);
+  }
+  catch(error) {
+    console.log(error)
+  }
+}
